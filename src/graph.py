c1bbb2d4a042dbc7e2e8b059536d6cb290ef94be
Javier Izquierdo Hernandez <javizqh@gmail.com>
2023-10-19 22:46:26 +0200

    Cleaning python code

--- src/graph.py
+++ src/graph.py
@@ -9,10 +9,11 @@ ax = fig.gca()
 data = pd.read_csv('cyclictestURJC.csv')
 data2 = pd.read_csv('cyclictestURJC1.csv')
 data3 = pd.read_csv('cyclictestURJC2.csv')
+
 # data['LATENCY'] = data['LATENCY'] / 1000000
 # data['LATENCY'].hist(bins=60, ax=ax, color='blue', edgecolor='black', range=[0, 11])
 
-# TODO: only for tests
+# TODO: only for tests change to above
 data['LATENCY'] = data['LATENCY'] / 100
 data['LATENCY'].hist(bins=100, ax=ax, range=[0, 50], color='blue', alpha=0.5, edgecolor='black', label="Non Real Time - idle")
 data2['LATENCY'] = data2['LATENCY'] / 100
