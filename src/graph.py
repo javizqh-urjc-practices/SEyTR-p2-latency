f47c56464c7000cf823a2faba983c53bf105c6da
Javier Izquierdo Hernandez <javizqh@gmail.com>
2023-10-24 14:53:09 +0200

    Adding results from rbpi

--- src/graph.py
+++ src/graph.py
@@ -6,22 +6,19 @@ import numpy as np
 fig = plt.figure(figsize=(8,3))
 ax = fig.gca()
 
-data = pd.read_csv('cyclictestURJC.csv')
-data2 = pd.read_csv('cyclictestURJC1.csv')
-data3 = pd.read_csv('cyclictestURJC2.csv')
+data = pd.read_csv('noRtBonnie.csv')
+data2 = pd.read_csv('noRtHackbench.csv')
+data3 = pd.read_csv('noRtIdle.csv')
 
-# data['LATENCIA'] = data['LATENCIA'] / 1000000
-# data['LATENCIA'].hist(bins=60, ax=ax, color='blue', edgecolor='black', range=[0, 11])
+data['LATENCIA'] = data['LATENCIA'] / 1000000
+data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='blue', alpha=0.5, edgecolor='black', label="Non Real Time - bonnie++")
+data2['LATENCIA'] = data2['LATENCIA'] / 1000000
+data2['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='red', alpha=0.5, edgecolor='black', label="Non Real Time - hackbench")
+data3['LATENCIA'] = data3['LATENCIA'] / 1000000
+data3['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='green', alpha=0.5, edgecolor='black', label="Non Real Time - idle")
 
-# TODO: only for tests change to above
-data['LATENCIA'] = data['LATENCIA'] / 100
-data['LATENCIA'].hist(bins=100, ax=ax, range=[0, 50], color='blue', alpha=0.5, edgecolor='black', label="Non Real Time - bonnie++")
-data2['LATENCIA'] = data2['LATENCIA'] / 100
-data2['LATENCIA'].hist(bins=100, ax=ax, range=[0, 50], color='red', alpha=0.5, edgecolor='black', label="Non Real Time - hackbench")
-data3['LATENCIA'] = data3['LATENCIA'] / 100
-data3['LATENCIA'].hist(bins=100, ax=ax, range=[0, 50], color='green', alpha=0.5, edgecolor='black', label="Non Real Time - idle")
-plt.legend(loc="upper right")
-plt.ylabel('Frecuencia')
-plt.xlabel('latencia (microsegundos)')
-plt.title('cyclictestURJC')
+plt.legend(loc="upper right", prop={'size':30})
+plt.ylabel('Frecuencia', fontsize=18)
+plt.xlabel('latencia (microsegundos)', fontsize=18)
+plt.title('cyclictestURJC RaspberryPi - Kernel No RT', fontsize=20)
 plt.show()
